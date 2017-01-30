f1be85e580b004683310a245b2e37e5b680ce406
Jordan Gosney <wizofgoz2@gmail.com>
2017-01-30 08:58:39 -0600

    Update CanStoreInterface.php

--- src/Manager/Session/CanStoreInterface.php
+++ src/Manager/Session/CanStoreInterface.php
@@ -2,6 +2,11 @@
 namespace Esir\Manager\Session;
 interface CanStoreInterface
 {
-	public function __get($name);
-	public function __set($name, $value);
+	public function __construct($name, $limit = 0, $path = '/', $domain = NULL, $secure = NULL);
+	public function startSession();
+	public function __set( $name , $value );
+	public function __get( $name );
+	public function __isset( $name );
+	public function __unset( $name );
+	public function destroy();
 }
